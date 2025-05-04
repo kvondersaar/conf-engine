13139207edd0f73f136d1774e6cb61c676951ac7
kvondersaar <54597358+kvondersaar@users.noreply.github.com>
2025-05-04 07:36:00 -0500

    Fixup license, docs, and versioning

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -50,8 +50,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['test_*', 'conftest.py']
-
+autoapi_ignore = ['*/tests/*', '*/conftest.py']
 
 # -- Options for HTML output -------------------------------------------------
 
